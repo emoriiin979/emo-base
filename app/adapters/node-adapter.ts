b0dc2d05fb934dcb9127ca336ffe143c1d861024
emoriiin979 <emoriiin979@gmail.com>
2025-08-08 02:56:58 +0900

    adapterをindex.tsから呼び出す方式に変更

--- app/adapters/node-adapter.ts
+++ app/adapters/node-adapter.ts
@@ -1,12 +1,13 @@
+import type { Hono } from 'hono'
 import { serve } from '@hono/node-server'
-import { serveStatic } from '@hono/node-server/serve-static'
-import app from '../index.js'
 
-app.use('/*', serveStatic({ root: './public' }))
-
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+export const handle = async (
+  app: Hono,
+) => {
+  serve({
+    fetch: app.fetch,
+    port: 3000,
+  }, (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
+  })
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,27 @@
 import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
+import FileStore from './session/store/FileStore.js'
+import { sessionMiddleware } from 'hono-sessions'
 import authRoutes from './auth/auth-route.js'
+import { Header } from './components/header.js'
+import { handle } from './adapters/node-adapter.js'
 
 const app = new Hono()
+
+app.use('/*', serveStatic({ root: './public' }))
+
+const store = new FileStore()
+app.use('/*', sessionMiddleware({
+  store,
+  expireAfterSeconds: 900,
+}))
 
 app.route('/auth', authRoutes)
 
 app.get('/', (c) => {
   return c.render(
     <html>
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>emo-base</title>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      </head>
+      <Header title="TOP" />
       <body>
         <h1>Hello Hono!</h1>
       </body>
@@ -21,4 +29,4 @@ app.get('/', (c) => {
   )
 })
 
-export default app
+handle(app)
